788e1371e19bf55b9044d0499775c1632f0c51d0
angelorubin <angelorubin@gmail.com>
2021-06-21 07:37:51 -0300

    Removendo reducer não utilizado da store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import feat01Reducer from "features/feat-01/store/reducer";
-import feat02Reducer from "features/feat-02/store/reducer";
 
 const reducer = {
   feat01: feat01Reducer,
-  feat02: feat02Reducer,
 };
 
 export default configureStore({
