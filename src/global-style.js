28cf7a9350ebe05deda5508a70c7fdd7a9692a37
Angelo Rubin <angelorubin@gmail.com>
2021-06-21 17:44:06 -0300

    Refatorando CSS global

--- src/global-style.js
+++ src/global-style.js
@@ -1,17 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap');
-    :root {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-        font-family: 'Roboto', sans-serif;
-        /** Colors */
-        --primary-color: blue;
-    }
-    /** Sizes */
-    --smallSize: 1rem;
-    --mediumSize: 1.5rem;
-    --largeSize: 2rem;
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap');
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    // 1rem === 10px
+    font-size: 62.5%;
+  }
+
+  html, body {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  :root{
+    // Colors
+    --primary-color: pink;
+  }
 `;
