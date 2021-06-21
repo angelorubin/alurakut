64ecfd9338f2b64de8bd92f09fb4ef1d9fc2d84c
angelorubin <angelorubin@gmail.com>
2021-06-21 07:39:05 -0300

    Testando o estado global em duas paginas diferentes

--- src/features/feat-01/views/feat01Container.jsx
+++ src/features/feat-01/views/feat01Container.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import * as S from "./feat01Styles";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Feat01 = () => {
-  const state = useSelector((state) => state.feat01);
+  const { title } = useSelector((state) => state.feat01);
   const dispatch = useDispatch();
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+
     return dispatch({
       type: "feat01/addTitle",
-      payload: "React Redux",
+      payload: "React Redux Boilerplate",
     });
   };
 
   return (
     <S.Container>
-      <S.Title>{state.title}</S.Title>
-      <h1>lorem ipsun dolor</h1>
-      <button onClick={handleClick}>modificar titulo</button>
+      <nav>
+        <Link to="/feat02">feat 02</Link>
+      </nav>
+      <S.Title>{title}</S.Title>
+      <h3>Page - Feat 01</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit repellat
+        dolor eos repellendus cumque necessitatibus quas suscipit magni eveniet
+        ducimus excepturi beatae architecto tempora quod consequatur, modi
+        voluptates voluptatum porro.
+      </p>
+      <button type="button" onClick={handleClick}>
+        modificar titulo
+      </button>
     </S.Container>
   );
 };
--- src/features/feat-02/views/feat02Container.jsx
+++ src/features/feat-02/views/feat02Container.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import * as S from "./feat02Styles";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Feat01 = () => {
-  const state = useSelector((state) => state.feat01);
-  const dispatch = useDispatch();
-
-  const handleClick = () => {
-    return dispatch({
-      type: "feat02/addTitle",
-      payload: "React Redux",
-    });
-  };
+  const { title } = useSelector((state) => state.feat01);
 
   return (
     <S.Container>
-      <S.Title>{state.title}</S.Title>
-      <h1>lorem ipsun dolor</h1>
-      <button onClick={handleClick}>modificar titulo</button>
+      <S.Title>{title}</S.Title>
+      <h3>Page - Feat 02</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at,
+        nam architecto facere corrupti excepturi dolor cum reprehenderit iure
+        pariatur, consectetur accusamus adipisci, nemo voluptate voluptatum.
+        Distinctio hic inventore quibusdam!
+      </p>
     </S.Container>
   );
 };
