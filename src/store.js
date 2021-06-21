74b42161aa77817ecd48b2d46f634322710f10b0
Angelo Rubin <angelorubin@gmail.com>
2021-06-21 17:44:31 -0300

    Renomeando reducer

--- src/store.js
+++ src/store.js
@@ -2,11 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import feat01Slicer from "features/feat-01/store/slice";
 import feat02Slicer from "features/feat-02/store/slice";
 
-const reducers = {
+const reducer = {
   feat01: feat01Slicer,
   feat02: feat02Slicer,
 };
 
 export default configureStore({
-  reducer: reducers,
+  reducer,
 });
