13cfdb01da16b5adca1b88ebd90a3dc38126cfe5
angelorubin <angelorubin@gmail.com>
2021-06-21 07:36:56 -0300

    Organizando o roteamento da aplicação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Feat01 from "./features/feat-01";
 import Feat02 from "./features/feat-02";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
-    <>
-      <Feat01 />
-      <Feat02 />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Feat01 />
+        </Route>
+        <Route path="/feat02">
+          <Feat02 />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
