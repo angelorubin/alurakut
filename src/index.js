91e01f3a1230867c72afbdfed3061f2e2b123a14
Angelo Rubin <angelorubin@gmail.com>
2021-06-23 17:57:09 -0300

    Importando Reset do Material-UI

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import { Provider } from "react-redux";
 // Redux Store
 import store from "./store";
 import { theme } from "themes/default";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </Provider>
