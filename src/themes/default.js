cc27be95c61f1222e69745be12b10fb6ed8edc70
Angelo Rubin <angelorubin@gmail.com>
2021-06-23 18:00:05 -0300

    Adicionando breakpoints ao tema

--- src/themes/default.js
+++ src/themes/default.js
@@ -1,13 +1,36 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { blue, green } from "@material-ui/core/colors";
+import { red, blue, green } from "@material-ui/core/colors";
 
 export const theme = createMuiTheme({
+  breakpoints: {
+    keys: {
+      0: "xs",
+      1: "sm",
+      2: "md",
+      3: "lg",
+      4: "xl",
+    },
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
   palette: {
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
     primary: {
-      main: blue[900],
+      main: blue[600],
     },
     secondary: {
       main: green[600],
+    },
+    warning: {
+      main: red[500],
     },
   },
 });
