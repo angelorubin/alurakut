a8e6949c1b5a5d5e7ecae22d3a5deb81a5b97c63
angelorubin <angelorubin@gmail.com>
2021-06-24 21:13:13 -0300

    Removendo import da fonte do google e colocando na index com o react-helmet

--- src/global-style.js
+++ src/global-style.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap');
-
   :root{}
 
   * {
