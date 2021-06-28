aa4a56a6d1d560931baadfa866d5c1e9dbab8545
angelorubin <angelorubin@gmail.com>
2021-06-27 23:53:29 -0300

    Implementando o componente Header

---
+++ src/shared/components/header/index.js
@@ -0,0 +1,71 @@
+import * as React from "react";
+import { AppBar, Typography, Box, SvgIcon } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import * as S from "./styles";
+import { ExitToApp } from "@material-ui/icons";
+
+export const Header = () => {
+  const { title } = useSelector((state) => state.dashboard);
+  return (
+    <AppBar
+      position="relative"
+      sx={{
+        minHeight: "10vh",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 1,
+      }}
+    >
+      <Box sx={{ flex: 1 }}>{title}</Box>
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          gap: 1,
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <S.Link to="/">
+          <Typography
+            variant="body2"
+            sx={{
+              color: "common.white",
+            }}
+          >
+            Home
+          </Typography>
+        </S.Link>
+        <S.Link to="/">
+          <Typography
+            variant="body2"
+            sx={{
+              color: "common.white",
+            }}
+          >
+            About Us
+          </Typography>
+        </S.Link>
+        <S.Link to="/">
+          <Typography
+            variant="body2"
+            sx={{
+              color: "common.white",
+            }}
+          >
+            link 01
+          </Typography>
+        </S.Link>
+        <S.Link
+          to="/"
+          sx={{
+            color: "common.white",
+          }}
+        >
+          <ExitToApp />
+        </S.Link>
+      </Box>
+    </AppBar>
+  );
+};
---
+++ src/shared/components/header/styles.js
@@ -0,0 +1,6 @@
+import { styled } from "@material-ui/core";
+import { Link as ReactRouterDomLink } from "react-router-dom";
+
+export const Link = styled(ReactRouterDomLink)`
+  text-decoration: none;
+`;
