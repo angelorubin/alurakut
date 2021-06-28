1e9f6817b32462ddcea2291fe66758a5a8922478
angelorubin <angelorubin@gmail.com>
2021-06-27 23:45:04 -0300

    Adicionando reducer Dashboard na store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import feat01Slicer from "features/home/store/slice";
-import feat02Slicer from "features/feat-02/store/slice";
+import dashboardSlicer from "features/dashboard/store/slice";
 
 const reducer = {
-  feat01: feat01Slicer,
-  feat02: feat02Slicer,
+  dashboard: dashboardSlicer,
 };
 
 export default configureStore({
