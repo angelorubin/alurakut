f9f86f9bb39f3ca2a54685f65f93e8f9e6f72182
Angelo Rubin <angelorubin@gmail.com>
2021-07-01 18:06:03 -0300

    Implementando messages reducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dashboardSlicer from "features/dashboard/store/slice";
+import messagesSlicer from "features/messages/store/slice";
 
 const reducer = {
   dashboard: dashboardSlicer,
+  messages: messagesSlicer,
 };
 
 export default configureStore({
