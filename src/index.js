8495cdf3077a3c05d4c301821c41c9e591277c1c
Angelo Rubin <angelorubin@gmail.com>
2021-07-01 18:05:26 -0300

    Organizando imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { ThemeProvider } from "@material-ui/core/styles";
 import ReactDOM from "react-dom";
 import { App } from "./app";
-import { GlobalStyle } from "./global-style.js";
+// import { GlobalStyle } from "./global-style.js";
 import { Provider } from "react-redux";
+
 // Redux Store
 import store from "./store";
+
+// Theme
 import { theme } from "themes/default";
+
+// Material-UI Reset
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { Helmet } from "react-helmet";
+
+// Router
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
