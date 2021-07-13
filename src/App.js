1a6a1c48c4540ab282d46573d750f65d41b2d0b5
Angelo Rubin <angelorubin@gmail.com>
2021-07-13 09:17:43 -0300

    Renomeando rota para user

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import { useRoutes } from "react-router-dom";
 import Dashboard from "./features/dashboard";
 import SignIn from "./features/signin";
 import SignUp from "./features/signup";
-import Messages from "./features/messages";
+import User from "./features/user";
 
 export const App = () => {
-  let routes = useRoutes([
-    {
-      path: "/",
-      element: <SignIn />,
-    },
-    {
-      path: "/signup",
-      element: <SignUp />,
-    },
-    {
-      path: "/dashboard",
-      element: <Dashboard />,
-      children: [
-        {
-          path: "messages",
-          element: <Messages />,
-        },
-      ],
-    },
-  ]);
-  return routes;
+	let routes = useRoutes([
+		{
+			path: "/",
+			element: <SignIn />,
+		},
+		{
+			path: "/signup",
+			element: <SignUp />,
+		},
+		{
+			path: "/dashboard",
+			element: <Dashboard />,
+			children: [
+				{
+					path: "user",
+					element: <User />,
+				},
+			],
+		},
+	]);
+	return routes;
 };
