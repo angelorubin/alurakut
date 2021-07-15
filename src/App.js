b6ce207edd06f3d9484b8fd988ad932717df6bdc
Angelo Rubin <angelorubin@gmail.com>
2021-07-15 15:57:45 -0300

    Separando arquivo de rotas

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { useRoutes } from "react-router-dom";
-import Dashboard from "./features/dashboard";
-import SignIn from "./features/signin";
-import SignUp from "./features/signup";
-import User from "./features/user";
+import { Routes } from "routes";
 
 export const App = () => {
-	let routes = useRoutes([
-		{
-			path: "/",
-			element: <SignIn />,
-		},
-		{
-			path: "/signup",
-			element: <SignUp />,
-		},
-		{
-			path: "/dashboard",
-			element: <Dashboard />,
-			children: [
-				{
-					path: "/user",
-					element: <User />,
-				},
-			],
-		},
-	]);
-	return routes;
+	return <Routes />;
 };
