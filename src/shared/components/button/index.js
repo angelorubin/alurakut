65bbc5ed025e2b1db4b0248774906a28609c25f5
Angelo Rubin <angelorubin@gmail.com>
2021-07-17 14:54:48 -0300

    Renomeando extensão do arquivo de .js para .jsx

--- src/shared/components/button/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import * as S from "./styles";
-
-export const Button = (props) => {
-  return <S.BaseButton {...props}>{props.children}</S.BaseButton>;
-};
--- src/shared/components/container/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import * as S from "./styles";
-
-export const Container = (props) => {
-  return <S.Container {...props}>{props.children}</S.Container>;
-};
