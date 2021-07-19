1a5d7fcf256d05db10306b814b913e16ed031b65
Angelo Rubin <angelorubin@gmail.com>
2021-07-19 11:44:10 -0300

    Refatorando layout do componente signin

--- src/features/signin/views/signin.jsx
+++ src/features/signin/views/signin.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {
-  Box,
-  Typography,
-  Link,
-  Button,
-  Divider,
-  OutlinedInput,
+	Box,
+	Typography,
+	Link,
+	Button,
+	Divider,
+	OutlinedInput,
 } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
@@ -13,347 +13,333 @@ import LoginLogo from "assets/icons/login-logo.svg";
 import * as S from "./styles";
 
 const Signin = () => {
-  let navigate = useNavigate();
+	let navigate = useNavigate();
 
-  const { handleSubmit, handleChange, values } = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      if (values) {
-        console.log(JSON.stringify(values, null, 2));
-        navigate("/dashboard", { replace: true });
-      }
-    },
-  });
+	const { handleSubmit, handleChange, values } = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		onSubmit: (values) => {
+			if (values) {
+				console.log(JSON.stringify(values, null, 2));
+				navigate("/dashboard", { replace: true });
+			}
+		},
+	});
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        backgroundColor: "primary.light",
-        overflowY: "hidden",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: (theme) => theme.spacing(2),
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flex: 1,
-            gap: (theme) => theme.spacing(3),
-            "@media (max-width: 600px)": {
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-            },
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              gap: (theme) => theme.spacing(4),
-              backgroundColor: "common.white",
-              borderRadius: 8,
-              height: "368px",
-              flex: 4,
-            }}
-          >
-            <S.Logo
-              src={LoginLogo}
-              sx={{
-                maxWidth: "319px",
-                height: "76px",
-                "@media (max-width: 400px)": {
-                  width: "190px",
-                  height: "45px",
-                },
-              }}
-            />
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: (theme) => theme.spacing(2),
-              }}
-            >
-              <Typography
-                sx={{
-                  fontFamily: (theme) => theme.typography.fontFamily.rubik,
-                  fontSize: "12px",
-                  mr: 4,
-                  ml: 4,
-                  mt: 4,
-                  textAlign: "center",
-                }}
-              >
-                <Typography
-                  sx={{
-                    color: "#D81D99",
-                    fontWeight: "bold",
-                    fontSize: "12px",
-                  }}
-                >
-                  Conecte-se
-                </Typography>
-                aos seus amigos e familiares usando recados e mensagens
-                instantâneas
-              </Typography>
-              <Typography
-                sx={{
-                  fontFamily: (theme) => theme.typography.fontFamily.rubik,
-                  fontSize: "12px",
-                  mr: 4,
-                  ml: 4,
-                  mt: 4,
-                  textAlign: "center",
-                }}
-              >
-                <Typography
-                  sx={{
-                    color: "#D81D99",
-                    fontSize: "12px",
-                    fontWeight: "bold",
-                  }}
-                >
-                  Conheça
-                </Typography>{" "}
-                novas pessoas através de amigos de seus amigos e comunidades
-              </Typography>
-              <Typography
-                sx={{
-                  fontFamily: (theme) => theme.typography.fontFamily.rubik,
-                  fontSize: "12px",
-                  mr: 4,
-                  ml: 4,
-                  mt: 4,
-                  textAlign: "center",
-                }}
-              >
-                <Typography
-                  sx={{
-                    color: "#D81D99",
-                    fontSize: "12px",
-                    fontWeight: "bold",
-                  }}
-                >
-                  Compartilhe
-                </Typography>
-                seus vídeos, fotos e paixões em um só lugar
-              </Typography>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flex: 1,
-              flexDirection: "column",
-            }}
-          >
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                flex: 3,
-                backgroundColor: "#F1F9FE",
-                gap: (theme) => theme.spacing(4),
-                width: "359px",
-                borderRadius: 8,
-                mb: 3,
-              }}
-            >
-              <Typography
-                sx={{
-                  fontSize: "14px",
-                  textAlign: "center",
-                }}
-              >
-                Acesse o
-                <Link
-                  href="http://www.orkut.com/index.html"
-                  underline="none"
-                  target="_blank"
-                  sx={{
-                    color: (theme) => theme.palette.grey[800],
-                    fontWeight: (theme) => theme.typography.fontWeightBold,
-                    fontFamily: (theme) => theme.typography.rubik,
-                    fontSize: "14px",
-                    mr: 1,
-                    ml: 1,
-                  }}
-                >
-                  www.orkut.com
-                </Link>
-                com a sua conta
-              </Typography>
-              <OutlinedInput
-                placeholder="Usuário"
-                sx={{
-                  background: " #FFFFFF",
-                  height: "40px",
-                  border: "1px solid #C5C6CA",
-                  borderRadius: "8px",
-                }}
-              />
-              <Button
-                sx={{
-                  backgroundColor: "#2E7BB4",
-                  width: "209px",
-                  height: "33px",
-                  color: (theme) => theme.palette.common.white,
-                  borderRadius: 4,
-                }}
-              >
-                Login
-              </Button>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                flex: 1,
-                alignItems: "center",
-                flexDirection: "column",
-                justifyContent: "center",
-                minHeight: "74px",
-                backgroundColor: "#F1F9FE",
-                borderRadius: 8,
-              }}
-            >
-              <Typography
-                sx={{
-                  fontFamily: (theme) => theme.typography.rubik,
-                  fontsize: "12px",
-                  lineHeight: "14px",
-                  textAlign: "center",
-                  color: "#333333",
-                }}
-              >
-                Ainda não é membro?
-              </Typography>
-              <Typography sx={{}}>ENTRAR JÁ</Typography>
-            </Box>
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: (theme) => theme.shape.borderRadius(8),
-            width: "100%",
-            backgroundColor: "#BBCDE8",
-            minHeight: "27px",
-            p: 1,
-          }}
-        >
-          <Typography
-            sx={{
-              display: "flex",
-              fontFamily: (theme) => theme.palette.common.white,
-              fontSize: "12px",
-              lineHeight: "14px",
-              textAlign: "center",
-              "@media (max-width:400px)": {
-                display: "flex",
-                flexDirection: "column",
-                gap: "1rem",
-              },
-            }}
-          >
-            <Typography
-              component="span"
-              sx={{
-                fontFamily: (theme) => theme.typography.rubik,
-                fontSize: "12px",
-                lineHeight: "14px",
-                textAlign: "center",
-                color: "#333333",
-              }}
-            >
-              © 2020 Orkut.br
-            </Typography>
-            <Divider
-              sx={{
-                "@media (max-width: 400px)": {
-                  display: "none",
-                },
-              }}
-            >
-              -
-            </Divider>
-            <Link href="#" underline="none">
-              Sobre o Orkut.br
-            </Link>
-            <Divider
-              sx={{
-                "@media (max-width: 400px)": {
-                  display: "none",
-                },
-              }}
-            >
-              -
-            </Divider>
-            <Link href="#" underline="none">
-              Centro de segurança
-            </Link>
-            <Divider
-              sx={{
-                "@media (max-width: 400px)": {
-                  display: "none",
-                },
-              }}
-            >
-              -
-            </Divider>
-            <Link href="#" underline="none">
-              Privacidade
-            </Link>
-            <Divider
-              sx={{
-                "@media (max-width: 400px)": {
-                  display: "none",
-                },
-              }}
-            >
-              -
-            </Divider>
-            <Link href="#" underline="none">
-              Termos
-            </Link>
-            <Divider
-              sx={{
-                "@media (max-width: 400px)": {
-                  display: "none",
-                },
-              }}
-            >
-              -
-            </Divider>
-            <Link href="#" underline="none">
-              Contato
-            </Link>
-          </Typography>
-        </Box>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				minHeight: "100vh",
+				backgroundColor: "primary.light",
+				overflowY: "hidden",
+			}}
+		>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					gap: (theme) => theme.spacing(2),
+					m: 4,
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						flex: 1,
+						gap: (theme) => theme.spacing(3),
+						"@media (max-width: 600px)": {
+							display: "flex",
+							flexDirection: "column",
+							gap: (theme) => theme.spacing(3),
+						},
+					}}
+				>
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							justifyContent: "center",
+							gap: (theme) => theme.spacing(4),
+							backgroundColor: "common.white",
+							borderRadius: 8,
+							height: "368px",
+							flex: 4,
+							p: 4,
+						}}
+					>
+						<S.Logo
+							src={LoginLogo}
+							sx={{
+								maxWidth: "319px",
+								height: "76px",
+								"@media (max-width: 400px)": {
+									width: "190px",
+									height: "45px",
+								},
+							}}
+						/>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+								justifyContent: "center",
+								gap: (theme) => theme.spacing(2),
+							}}
+						>
+							<Typography
+								sx={{
+									fontFamily: (theme) => theme.typography.fontFamily.rubik,
+									fontSize: "12px",
+									mr: 4,
+									ml: 4,
+									mt: 4,
+									textAlign: "center",
+								}}
+							>
+								<Typography
+									component="span"
+									sx={{
+										color: "#D81D99",
+										fontWeight: "bold",
+										fontSize: "12px",
+										mr: 1,
+									}}
+								>
+									Conecte-se
+								</Typography>
+								aos seus amigos e familiares usando recados e mensagens
+								instantâneas
+							</Typography>
+							<Typography
+								sx={{
+									fontFamily: (theme) => theme.typography.fontFamily.rubik,
+									fontSize: "12px",
+									mr: 4,
+									ml: 4,
+									mt: 4,
+									textAlign: "center",
+								}}
+							>
+								<Typography
+									component="span"
+									sx={{
+										color: "#D81D99",
+										fontSize: "12px",
+										fontWeight: "bold",
+										mr: 1,
+									}}
+								>
+									Conheça
+								</Typography>
+								novas pessoas através de amigos de seus amigos e comunidades
+							</Typography>
+							<Typography
+								sx={{
+									fontFamily: (theme) => theme.typography.fontFamily.rubik,
+									fontSize: "12px",
+									mr: 4,
+									ml: 4,
+									mt: 4,
+									textAlign: "center",
+								}}
+							>
+								<Typography
+									component="span"
+									sx={{
+										color: "#D81D99",
+										fontSize: "12px",
+										fontWeight: "bold",
+										mr: 1,
+									}}
+								>
+									Compartilhe
+								</Typography>
+								seus vídeos, fotos e paixões em um só lugar
+							</Typography>
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							flex: 1,
+							flexDirection: "column",
+						}}
+					>
+						<Box
+							sx={{
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								flexDirection: "column",
+								backgroundColor: "#F1F9FE",
+								gap: (theme) => theme.spacing(4),
+								borderRadius: 8,
+								mb: 3,
+								p: 4,
+							}}
+						>
+							<Typography
+								sx={{
+									fontSize: "14px",
+									textAlign: "center",
+								}}
+							>
+								Acesse o
+								<Link
+									href="http://www.orkut.com/index.html"
+									underline="none"
+									target="_blank"
+									sx={{
+										color: (theme) => theme.palette.grey[800],
+										fontWeight: (theme) => theme.typography.fontWeightBold,
+										fontFamily: (theme) => theme.typography.rubik,
+										fontSize: "14px",
+										mr: 1,
+										ml: 1,
+									}}
+								>
+									www.orkut.br
+								</Link>
+								com a sua conta
+							</Typography>
+							<OutlinedInput
+								sx={{
+									background: " #FFFFFF",
+									height: "40px",
+									border: "1px solid #C5C6CA",
+									borderRadius: 4,
+									fontFamily: (theme) => theme.typography.fontFamily.rubik,
+								}}
+								placeholder="Usuário"
+							/>
+							<Button
+								sx={{
+									backgroundColor: "#2E7BB4",
+									width: "209px",
+									height: "40px",
+									color: "common.white",
+									borderRadius: 4,
+								}}
+							>
+								Login
+							</Button>
+						</Box>
+						<Box
+							sx={{
+								display: "flex",
+								flex: 1,
+								alignItems: "center",
+								flexDirection: "column",
+								justifyContent: "center",
+								minHeight: "74px",
+								backgroundColor: "#F1F9FE",
+								borderRadius: 8,
+							}}
+						>
+							<Typography
+								sx={{
+									fontFamily: (theme) => theme.typography.rubik,
+									fontSize: "12px",
+									lineHeight: "14px",
+									textAlign: "center",
+									color: "#333333",
+								}}
+							>
+								Ainda não é membro?
+							</Typography>
+							<Typography
+								sx={{
+									fontSize: "14px",
+									color: "primary.dark",
+									fontWeight: "bold",
+								}}
+							>
+								ENTRAR JÁ
+							</Typography>
+						</Box>
+					</Box>
+				</Box>
+				<Box
+					sx={{
+						display: "flex",
+						flex: 1,
+						alignItems: "center",
+						justifyContent: "center",
+						borderRadius: (theme) => theme.shape.borderRadius(8),
+						width: "100%",
+						backgroundColor: "#BBCDE8",
+						minHeight: "27px",
+						p: 1,
+					}}
+				>
+					<Typography
+						sx={{
+							display: "flex",
+							fontFamily: (theme) => theme.palette.common.white,
+							fontSize: "12px",
+							lineHeight: "14px",
+							textAlign: "center",
+							"@media (max-width:400px)": {
+								display: "flex",
+								flexFlow: "row wrap",
+								alignItems: "center",
+								justifyContent: "center",
+								gap: (theme) => theme.spacing(1),
+							},
+						}}
+					>
+						<Typography
+							component="span"
+							sx={{
+								fontFamily: (theme) => theme.typography.rubik,
+								fontSize: "12px",
+								lineHeight: "14px",
+								textAlign: "center",
+								color: "#333333",
+							}}
+						>
+							© 2020 Orkut.br
+						</Typography>
+						<Divider
+							sx={{
+								"@media (max-width: 400px)": {
+									display: "none",
+								},
+							}}
+						>
+							-
+						</Divider>
+						<Link href="#" underline="none">
+							Sobre o Orkut.br
+						</Link>
+						<Divider>-</Divider>
+						<Link href="#" underline="none">
+							Centro de segurança
+						</Link>
+						<Divider>-</Divider>
+						<Link href="#" underline="none">
+							Privacidade
+						</Link>
+						<Divider>-</Divider>
+						<Link href="#" underline="none">
+							Termos
+						</Link>
+						<Divider>-</Divider>
+						<Link href="#" underline="none">
+							Contato
+						</Link>
+					</Typography>
+				</Box>
+			</Box>
+		</Box>
+	);
 };
 
 export default Signin;
